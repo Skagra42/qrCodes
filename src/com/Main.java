b955fc05489cc2d81a31aa195ef8b18477f3815a
Skagra42 <flynnsal@colostate.edu>
2022-05-16 19:27:58 -0600

    Setting up Main.

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,20 +2,12 @@ package com;
 
 public class Main {
 	public static void main (String [] args) {
-		String [] inputs;
-		
 		for (int a = 0; a < args.length; a++) {
-			System.out.println (args [a]);
+			makeCode (args [a]);
 		}
 	}
 
-	public void makeCodes (String [] inputs) {
-		for (int i = 0; i < inputs.length; i++) {
-			makeCode (inputs [i]);
-		}
-	}
-
-	public void makeCode (String input) {
+	public static void makeCode (String input) {
 
 	}
 }
