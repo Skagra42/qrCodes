638015cc64d7985ae18376fc95ea49e209b3d495
Skagra42 <flynnsal@colostate.edu>
2022-05-16 19:13:42 -0600

    1

---
+++ src/com/Main.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Main {
+	public static void main (String [] args) {
+		String [] inputs;
+		
+		for (int a = 0; a < args.length; a++) {
+			System.out.println (args [a]);
+		}
+	}
+
+	public void makeCodes (String [] inputs) {
+		for (int i = 0; i < inputs.length; i++) {
+			makeCode (inputs [i]);
+		}
+	}
+
+	public void makeCode (String input) {
+
+	}
+}
