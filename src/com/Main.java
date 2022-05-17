9b345bd61102d2e4f6e3d7f376b99bd8e78a84da
Skagra42 <flynnsal@colostate.edu>
2022-05-16 20:54:29 -0600

    Added libraries and finished putting the arguments into makeCode.

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,13 +1,24 @@
 package com;
 
 public class Main {
+
+	//args [0] is the path of the file the codes will be put in.
+	//args [1] is the datatype.
+	//args [2] is the version.
+	//args [3] is the error correction.
+	//args [4+] are the Strings to turn into codes.
 	public static void main (String [] args) {
-		for (int a = 0; a < args.length; a++) {
-			makeCode (args [a]);
+		for (int a = 4; a < args.length; a++) {
+			makeCode (args [a], args [0], args [1], args [2], args [4]);
 		}
 	}
 
-	public static void makeCode (String input) {
-
+	public static void makeCode (String input, String filePath, String datatype, String version, String errorCorrection) {
+		try {
+			
+		}
+		catch (Exception e) {
+			System.err.println (e);
+		}
 	}
 }
