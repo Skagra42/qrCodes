1b1090ced6906defb96f9b2f2c090e7b2a0a3763
Skagra42 <flynnsal@colostate.edu>
2022-05-20 20:00:17 -0600

    Created QRCodeData.java.

---
+++ src/com/QRCodeExtended.java
@@ -0,0 +1,9 @@
+package com;
+
+import java.awt.image.BufferedImage;
+
+public class QRCodeExtended extends BufferedImage {
+    public QRCodeExtended(int width, int height, int imageType) {
+        super(width, height, imageType);
+    }
+}
